410a946b7e5f5c80a087b019ab2b5d6b10291ef2
Maurisio Esparza <63465861+esparz50@users.noreply.github.com>
2020-07-26 18:30:12 -0700

    Add files via upload
    Uploaded the python program. It is ready to go!

---
+++ Mad Libs Game Tech Community Edition.py
@@ -0,0 +1,7 @@
+
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural: ")
+celebrity = input("Enter a tech celebrity from YouTube: ")
+print("PC and accessories are in  " + color)
+print(plural_noun + " are overclocking")
+print("Stan " + celebrity)
